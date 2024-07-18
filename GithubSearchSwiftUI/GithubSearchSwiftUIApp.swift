3c1d59188e5d894017580517fb0081924b01914c
Jboy422 <gitonly543@gmail.com>
2024-07-18 09:15:13 +0900

    Initial Commit

---
+++ GithubSearchSwiftUI/GithubSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchSwiftUIApp.swift
+//  GithubSearchSwiftUI
+//
+//  Created by Jboy422 on 2024/07/18.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
